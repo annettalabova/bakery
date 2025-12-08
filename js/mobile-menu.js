c302d7bc249045bafc4d8af32ecc6ba37415ac7f
Anna Talabova <talabova@yahoo.com>
2025-12-07 22:23:05 -0700

    finish project mobile and modal menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,9 +2,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const mobileMenu = document.querySelector('.mobile-menu');
   const menuBtnOpen = document.querySelector('.menu-btn-open');
   const menuBtnClose = document.querySelector('.menu-btn-close');
+  const burger = document.querySelector('.header__burger');
+  const menuLinks = document.querySelectorAll('.mobile-menu__link');
+  const html = document.documentElement; // ← вот это добавили
 
-  const toggleMobileMenu = () => mobileMenu.classList.toggle('is-open');
+  if (!mobileMenu || !menuBtnOpen || !menuBtnClose) return;
+
+  const toggleMobileMenu = () => {
+    mobileMenu.classList.toggle('is-open');
+    document.body.classList.toggle('no-scroll');
+    html.classList.toggle('no-scroll'); // ← это добавили
+    burger && burger.classList.toggle('is-open');
+  };
 
   menuBtnOpen.addEventListener('click', toggleMobileMenu);
   menuBtnClose.addEventListener('click', toggleMobileMenu);
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', toggleMobileMenu);
+  });
 });
--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,14 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnsOpen = document.querySelectorAll('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
-const toggleModal = () => modal.classList.toggle('is-hidden');
+const toggleModal = () => {
+  modal.classList.toggle('is-hidden');
+  document.body.classList.toggle('no-scroll');
+};
 
-modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnsOpen.forEach(btn => {
+  btn.addEventListener('click', toggleModal);
+});
+
 modalBtnClose.addEventListener('click', toggleModal);
